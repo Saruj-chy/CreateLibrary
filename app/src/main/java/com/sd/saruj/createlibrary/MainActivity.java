5fb0c7dab11d056f8e6f55964a826300b3445dd2
Saruj-Chy <saruj.chy222@gmail.com>
2020-07-02 20:33:19 +0600

    error dectect

--- app/src/main/java/com/sd/saruj/createlibrary/MainActivity.java
+++ app/src/main/java/com/sd/saruj/createlibrary/MainActivity.java
@@ -3,6 +3,9 @@ package com.sd.saruj.createlibrary;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+
+import com.sd.saruj.mymath.OwnMath;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        int value = OwnMath.plus(5,10);
+        Log.d("TAG","value: "+value) ;
     }
 }
